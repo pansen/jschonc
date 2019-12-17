a0b988d10a590034d858f9128e0838bfce6febb9
andi <andi@zerotired.com>
2019-12-17 22:18:02 +0000

    Do some proto-rendering

--- src/components/OutputRenderer.vue
+++ src/components/OutputRenderer.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="zt-output-renderer">
-    <pre>{{ $props.object_ }}</pre>
+    <pre>{{ rendered }}</pre>
   </div>
 </template>
 
 <script>
+  function renderSchemaClass (className, jsonProcessed, indent) {
+    const _intent = 4;
+    let def = `${' '.repeat(indent * _intent)}from marshmallow import Schema, fields\n`;
+    def += `${' '.repeat(indent * _intent)}class ${className}(Schema):\n`;
+    for (const k in jsonProcessed) {
+      const v = jsonProcessed[k];
+      const required = v._config_.nullable ? 'False' : 'True';
+      let fieldType;
+
+      if (typeof v.value === "number") {
+        if (typeof v.value === "number" && !Number.isInteger(v.value)) {
+          fieldType = 'Float';
+        } else {
+          fieldType = 'Integer';
+        }
+      } else {
+        fieldType = 'String';
+      }
+      def += `${' '.repeat((indent + 1) * _intent)}${k} = fields.${fieldType}(required=${required}, default='')\n`
+    }
+    return def
+  }
 
   export default {
     props: {
@@ -12,6 +34,12 @@
         required: true
       },
     },
+
+    computed: {
+      rendered () {
+        return renderSchemaClass('MyFirstSony', this.$props.object_, 0);
+      },
+    }
   }
 </script>
 
@@ -19,5 +47,9 @@
 <style scoped>
   div.filters {
     border: 1px solid #efefef;
+  }
+
+  div.zt-output-renderer pre {
+    text-align: left;
   }
 </style>
